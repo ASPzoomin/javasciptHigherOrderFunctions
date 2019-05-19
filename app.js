6ee5327fe08b6a0bf2ebea07f7d8ebb0650acb1b
pavan <pavanammula@gmail.com>
2019-05-19 12:37:22 +0530

    added new array called companies

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const ages=[1,2,3,4,5,6,7,8,9,34,54,23,65,4,8,33];
+const companies = [
+    {name:"companyOne",id:1,category:"Finance"},
+    {name:"companyTwo",id:2,category:"Retail"},
+    {name:"companyThree",id:3,category:"Technology"},
+    {name:"companyFour",id:4,category:"Finance"},
+    {name:"companyFive",id:5,category:"Retail"}
+]
 const canHaveSex = ages.filter(age => age >=19);
 console.log(canHaveSex);
+const RetailCompanies = companies.filter(company => company.category === "Retail");
+console.log(RetailCompanies);
